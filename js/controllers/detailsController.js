26506602fd19e733ca69c8b5fb0d0867be5fbd7b
Adam Cooke <adamcooke@webofwork.com>
2018-03-24 14:50:45 +0000

    combined app.js and main.js into one file

--- js/controllers/detailsController.js
+++ js/controllers/detailsController.js
@@ -1,41 +1,40 @@
 //********** home Controllers
-app.obj.mashupTemplateApp.controller( 'detailsController', function($scope, $rootScope,chartService, setUpService) {		
+app.controller('detailsController', function ($scope, $rootScope, chartService, setUpService) {
 	console.log('detailsController');
 
-	$(document).ready(function(){
+	$(document).ready(function () {
 		//highlight this as the selected page
 		$("#P1").addClass("active");
-		$("#P1").siblings().removeClass("active"); 
-		 
+		$("#P1").siblings().removeClass("active");
+
 		//clear selections from the detail page
 		chartService.clear('LEVEL4_MeasureName');
+
+
 
 		//hide toggle buttons
 		$("#toggleButtons").hide();
 	});
 
-	$scope.$on('LastRepeaterElement', function(){
-		chartService.getObject($rootScope.arrayObjectsDetailsController);	
-		chartService.modelKPI($rootScope.arrayKPIsCommon);
-	});	
+
 
 	$rootScope.Leve3Cathegory = [
 		{
-			Line:[
-				{Title: "Timeliness", Class: "L3BoxGreen L3Box", id: "DW01", qsListField:'=if(LEVEL4_Timeliness=1,LEVEL4_MeasureName)', qsFileldSelect:'LEVEL4_MeasureName'},
-				{Title: "Completeness", Class: "L3BoxGreen L3Box", id: "DW02", qsListField:'=if(LEVEL4_Completeness=1,LEVEL4_MeasureName)', qsFileldSelect:'LEVEL4_MeasureName'},
-				{Title: "Process Quality", Class: "L3BoxGreen L3Box", id: "DW03", qsListField:'=if([LEVEL4_Process Quality]=1,LEVEL4_MeasureName)', qsFileldSelect:'LEVEL4_MeasureName'},
-				{Title: "Productivity", Class: "L3BoxGreen L3Box", id: "DW04", qsListField:'=if(LEVEL4_Productivity=1,LEVEL4_MeasureName)', qsFileldSelect:'LEVEL4_MeasureName'},
+			Line: [
+				{ Title: "Timeliness", Class: "L3BoxGreen L3Box", id: "DW01", qsListField: '=if(LEVEL4_Timeliness=1,LEVEL4_MeasureName)', qsFileldSelect: 'LEVEL4_MeasureName' },
+				{ Title: "Completeness", Class: "L3BoxGreen L3Box", id: "DW02", qsListField: '=if(LEVEL4_Completeness=1,LEVEL4_MeasureName)', qsFileldSelect: 'LEVEL4_MeasureName' },
+				{ Title: "Process Quality", Class: "L3BoxGreen L3Box", id: "DW03", qsListField: '=if([LEVEL4_Process Quality]=1,LEVEL4_MeasureName)', qsFileldSelect: 'LEVEL4_MeasureName' },
+				{ Title: "Productivity", Class: "L3BoxGreen L3Box", id: "DW04", qsListField: '=if(LEVEL4_Productivity=1,LEVEL4_MeasureName)', qsFileldSelect: 'LEVEL4_MeasureName' },
 			],
 		},
 		{
-			Line:[
-				{Title: "Capacity", Class: "L3BoxGreen L3Box", id: "DW05", qsListField:'=if(LEVEL4_Capacity=1,LEVEL4_MeasureName)', qsFileldSelect:'LEVEL4_MeasureName'},
-				{Title: "Cost", Class: "L3BoxGreen L3Box", id: "DW06", qsListField:'=if(LEVEL4_Cost=1,LEVEL4_MeasureName)', qsFileldSelect:'LEVEL4_MeasureName'},
-				{Title: "Volume", Class: "L3BoxGreen L3Box", id: "DW07", qsListField:'=if(LEVEL4_Volume=1,LEVEL4_MeasureName)', qsFileldSelect:'LEVEL4_MeasureName'},
-				{Title: "People", Class: "L3BoxGreen L3Box", id: "DW08", qsListField:'=if(LEVEL4_People=1,LEVEL4_MeasureName)', qsFileldSelect:'LEVEL4_MeasureName'},
+			Line: [
+				{ Title: "Capacity", Class: "L3BoxGreen L3Box", id: "DW05", qsListField: '=if(LEVEL4_Capacity=1,LEVEL4_MeasureName)', qsFileldSelect: 'LEVEL4_MeasureName' },
+				{ Title: "Cost", Class: "L3BoxGreen L3Box", id: "DW06", qsListField: '=if(LEVEL4_Cost=1,LEVEL4_MeasureName)', qsFileldSelect: 'LEVEL4_MeasureName' },
+				{ Title: "Volume", Class: "L3BoxGreen L3Box", id: "DW07", qsListField: '=if(LEVEL4_Volume=1,LEVEL4_MeasureName)', qsFileldSelect: 'LEVEL4_MeasureName' },
+				{ Title: "People", Class: "L3BoxGreen L3Box", id: "DW08", qsListField: '=if(LEVEL4_People=1,LEVEL4_MeasureName)', qsFileldSelect: 'LEVEL4_MeasureName' },
 			],
 		},
-	];	
+	];
 });
 //********** End Summary Controllers
--- js/main.js
+++ js/main.js
@@ -1,77 +1,102 @@
+var configfile = {
+	"development": {
+		"host": "localhost",
+		"prefix": "/",
+		"port": 4848,
+		"isSecure": window.location.protocol === "https:"
+	},
+	"production": {
+		"host": "ausyd-feg1.qliktech.com",
+		"prefix": "/",
+		"port": 443,
+		"isSecure": window.location.protocol === "https:"
+
+	}
+}
+
+
+
+var config = configfile.development;
+var prefix = window.location.pathname.substr(0, window.location.pathname.toLowerCase().lastIndexOf("/extensions") + 1);
+
+var baseUrl = (config.isSecure ? "https://" : "http://") + config.host + (config.port ? ":" + config.port : "") + config.prefix + "resources";
 var scriptsUrl = 'http://localhost:4848/extensions/wow/';
 
 require.config({
-  baseUrl: "http://localhost:4848/resources",
-  paths: {
-  	'app': scriptsUrl + 'js/app',
-	'homeController': scriptsUrl + 'js/controllers/homeController',  	
-	'detailsController': scriptsUrl + 'js/controllers/detailsController',
-	'chartService': scriptsUrl + 'js/services/chartService',
-	'setUpService': scriptsUrl + 'js/services/setUpService',
-	'emitLastRepeaterElementDirective': scriptsUrl + 'js/directives/emitLastRepeaterElementDirective',  	
-	'dropdownDirective': scriptsUrl + 'js/directives/dropdownDirective', 
-  }
+	baseUrl: baseUrl,
+	paths: {
+		'homeController': scriptsUrl + 'js/controllers/homeController',
+		'detailsController': scriptsUrl + 'js/controllers/detailsController',
+		'chartService': scriptsUrl + 'js/services/chartService',
+		'setUpService': scriptsUrl + 'js/services/setUpService',
+		'dropdownDirective': scriptsUrl + 'js/directives/dropdownDirective',
+		'emitLastRepeaterElement': scriptsUrl + 'js/directives/emitLastRepeaterElement', 
+	}
 });
 
-requirejs.config(  
-{ config:{  
-text:{ useXhr:function(url, protocol, hostname, port){return true;}}  
-}  
-}); 
+var app = {};
+
+
 
 define([
-    'require',
-    'angular',	
-    'app'
-	], function (require, angular) {
-		'use strict';
-	
-		app.obj.mashupTemplateApp = angular.module( "mashupTemplateApp", ['ngRoute'] );
+	'require',
+	'angular'
+], function (require, angular) {
+	'use strict';
 
-		app.obj.mashupTemplateApp.config( function ( $routeProvider ) {
-			$routeProvider	
-			.when( '/details', {
-				templateUrl: 'views/details.html',
+	app = angular.module("mashupTemplateApp", ['ngRoute']);
+
+
+	app.config(function ($routeProvider) {
+		$routeProvider
+			.when('/details', {
+				templateUrl: 'js/views/details.html',
 				controller: 'detailsController'
-			} )				
-			.when( '/#', {
-				templateUrl: 'views/home.html',
+			})
+			.when('/#', {
+				templateUrl: 'js/views/home.html',
 				controller: 'homeController'
-			} )		
-			.when( '/home', {
-				templateUrl: 'views/home.html',
+			})
+			.when('/home', {
+				templateUrl: 'js/views/home.html',
 				controller: 'homeController'
-			} )						
-			.otherwise( {
-				templateUrl: 'views/home.html',
+			})
+			.otherwise({
+				templateUrl: 'js/views/home.html',
 				controller: 'homeController'
-			} );
-		} );
-
-
-
-		require([
-					'js/qlik',
-					'chartService',	
-					'setUpService',
-					'homeController',				
-					'detailsController',
-					'emitLastRepeaterElementDirective',
-					'dropdownDirective',
-				], function ( qlik ){
-
-			app.obj.qlik = qlik;
-
-			// bootstrap my angular application, including the "qlik-angular" module
-			// must be done before the Qlik Sense API is used
-			// you must also set qva-bootstrap="false" in your html file
-			angular.bootstrap( document, ["mashupTemplateApp", "qlik-angular"] );
-
-			qlik.setOnError( function ( error ) {
-				//TODO:bootstrap removes html elements on dismiss..should hide instead
-				$( "#errmsg" ).html( error.message ).parent().show();
-			} );
-
-			app.boot();
-		});		
+			});
 	});
+
+
+
+	require([
+		'js/qlik',
+		'chartService',
+		'setUpService',
+		'homeController',
+		'detailsController',
+		'dropdownDirective',
+		'emitLastRepeaterElement'
+	], function (qlik) {
+
+
+
+
+
+		// bootstrap my angular application, including the "qlik-angular" module
+		// must be done before the Qlik Sense API is used
+		// you must also set qva-bootstrap="false" in your html file
+
+
+		angular.bootstrap(document, ["mashupTemplateApp", "qlik-angular"])
+
+		app.qlik = qlik;
+
+		qlik.setOnError(function (error) {
+			//TODO:bootstrap removes html elements on dismiss..should hide instead
+			$("#errmsg").html(error.message).parent().show();
+		});
+
+
+	});
+});
